28883037aeb0c54a0540579d2fdf3999bd6afb32
newlog <newlog@overflowedminds.net>
2020-10-27 22:41:25 +0100

    Add dark theme

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Main from './components/MainComponent'
+import red from '@material-ui/core/colors/red';
+
+
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: red,
+    secondary: {
+      main: '#b9f6ca',
+    },
+  }
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <CssBaseline />
-        <Main />
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div>
+          <CssBaseline />
+          <Main />
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,66 @@
-import React, { Component } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from './HomeComponent'
 import Menu from './LeftMenuComponent';
 import '../App.css'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import * as THREE from 'three';
+import p5 from 'p5';
+import FOG from 'vanta/dist/vanta.fog.min';
+
+
+const Main = (props) => {
+    const [vantaEffect, setVantaEffect] = useState(0)
+    const myRef = useRef(null)
+    useEffect(() => {
+      if (!vantaEffect) {
+        setVantaEffect(FOG({
+            el: myRef.current,
+            THREE: THREE,
+            p5: p5,
+            
+        }))
+      }
+      return () => {
+        if (vantaEffect) vantaEffect.destroy()
+      }
+    }, [vantaEffect])
+
+    return (
+        <div ref={myRef}>
+            <div className="main">
+                <Header />
+                <Menu />
+                <Switch location={props.location}>
+                    <Route path="/home" component={HomePage} />
+                    <Redirect to="/home" />
+                </Switch>
+                <Footer />
+            </div>
+        </div>
+    )
+  }
+
+
+/*
 
 class Main extends Component {
+    constructor() {
+        super()
+        this.vantaRef = React.createRef()
+    }
+    componentDidMount() {
+        this.vantaEffect = BIRDS({
+            el: this.vantaRef.current,
+            THREE: THREE,
+            p5: p5,
+        })
+    }
+    componentWillUnmount() {
+        if (this.vantaEffect) this.vantaEffect.destroy()
+    }
+
     render() {
         const {
             location,
@@ -25,5 +79,6 @@ class Main extends Component {
         );
     }
 }
-
+*/
 export default Main;
+ 
